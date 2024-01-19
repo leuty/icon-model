af45772abd7acee7c3ff5500350652edd7c9d55d
Reiner Schnur <reiner.schnur@mpimet.mpg.de>
2024-01-19 12:52:00 +0000

    [mpim] Update and cleanup of cloud inhomogeneity factors
    https://gitlab.dkrz.de/icon/icon-mpim/-/merge_requests/323
    
    This MR adds a new inhomogeneity factor `cinhoms` for cloud snow in the `aes_cop_nml`
    namelist section.
    
    Also, previously there have been three parameters for cloud liquid water, `cinhoml[1-3]`,
    from which only `cinhoml1` was used. These are therefore now condensed into one parameter `cinhoml` .
    
    The default for all three parameters `cinhom[lis]` is 0.8.
    
    This will change results unless `cinhoms = 1.0` is set in the namelist.
    
    See task https://gitlab.dkrz.de/icon/icon-mpim/-/work_items/21 for discussion
    on the cloud inhomogeneity factors.
    
    Approved-by: Bjorn Stevens <bjorn.stevens@mpimet.mpg.de>
    Merged-by: Reiner Schnur <reiner.schnur@mpimet.mpg.de>

--- src/atm_phy_rte_rrtmgp/mo_rte_rrtmgp_interface.f90
+++ src/atm_phy_rte_rrtmgp/mo_rte_rrtmgp_interface.f90
@@ -33,7 +33,7 @@ MODULE mo_rte_rrtmgp_interface
   USE mo_icon_fluxes_sw,             ONLY: ty_icon_fluxes_sw, set_fractions
   USE mo_rte_rrtmgp_setup,           ONLY: k_dist_lw, k_dist_sw, &
                                            cloud_optics_lw, cloud_optics_sw, &
-                                           stop_on_err, inhoml, inhomi
+                                           stop_on_err, inhoml, inhomi, inhoms
 
   USE mo_rad_diag,                   ONLY: rad_aero_diag
   USE mo_timer,                      ONLY: ltimer, timer_start, timer_stop, &
@@ -1248,6 +1248,7 @@ CONTAINS
     !$ACC KERNELS DEFAULT(PRESENT) ASYNC(1)
     zlwp(:,:) = zlwp(:,:) * inhoml
     ziwp(:,:) = ziwp(:,:) * inhomi
+    zswp(:,:) = zswp(:,:) * inhoms
     !$ACC END KERNELS
     
     ! new cloud optics: allocate memory for cloud optical properties:
