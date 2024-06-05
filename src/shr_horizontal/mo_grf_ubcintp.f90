a4d2d83e3edec69e8c76ac5cd78ac9a2f6a5113d
Michael Krayer <michael.krayer@dwd.de>
2024-06-05 12:32:37 +0000

    [nwp] bugfix: present tables errors on Leonardo global runs
    See merge request icon/icon-nwp!1428
    
    Approved-by: Marek Jacob <1129-b380572@users.noreply.gitlab.dkrz.de>
    Merged-by: Daniel Reinert <daniel.reinert@dwd.de>

--- src/shr_horizontal/mo_grf_ubcintp.f90
+++ src/shr_horizontal/mo_grf_ubcintp.f90
@@ -90,7 +90,7 @@ SUBROUTINE interpol_vec_ubc(p_pp, p_pc, p_grf, p_vn_in, p_vn_out)
     npromz_ubcintp = nproma_ubcintp
   ENDIF
 
-  !$ACC DATA CREATE(vn_aux) IF(i_am_accel_node)
+  !$ACC DATA CREATE(vn_aux) IF(i_am_accel_node .AND. SIZE(vn_aux) > 0)
 
 !$OMP PARALLEL
 !$OMP DO PRIVATE (jb,je,nlen,nshift) ICON_OMP_DEFAULT_SCHEDULE
@@ -98,7 +98,7 @@ SUBROUTINE interpol_vec_ubc(p_pp, p_pc, p_grf, p_vn_in, p_vn_out)
     nlen = MERGE(nproma_ubcintp, npromz_ubcintp, jb /= nblks_ubcintp)
     nshift = (jb-1)*nproma_ubcintp
 
-    !$ACC PARALLEL DEFAULT(PRESENT) ASYNC(1) PRESENT(p_pp%edges%refin_ctrl) IF(i_am_accel_node)
+    !$ACC PARALLEL DEFAULT(PRESENT) ASYNC(1) PRESENT(p_pp%edges%refin_ctrl) IF(i_am_accel_node .AND. SIZE(vn_aux) > 0)
     !$ACC LOOP GANG VECTOR COLLAPSE(2)
     DO je = nshift+1, nshift+nlen
       DO jk = 1,2
