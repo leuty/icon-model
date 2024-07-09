374cde99b36d7b979da519f1b1ef6e0bbd348de6
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2024-07-09 14:17:08 +0000

    Overhaul the licensing
    https://gitlab.dkrz.de/icon/icon/-/merge_requests/395
    
    Add a reuse-based CI job to check licenses and make it pass.
    
    Co-authored-by: Ralf Mueller <ralf.mueller@dkrz.de>
    Merged-by: Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>

--- test/microphysics_1mom_schemes/cmake/Modules/FindNetCDF.cmake
+++ test/microphysics_1mom_schemes/cmake/Modules/FindNetCDF.cmake
@@ -5,6 +5,8 @@
 # In applying this licence, ECMWF does not waive the privileges and immunities
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
+#
+# SPDX-License-Identifier: Apache-2.0
 
 # Try to find NetCDF includes and library
 #
