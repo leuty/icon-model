507c565923ab05c9f225392a0040042feeda3670
Jochen Förstner <jochen.foerstner@dwd.de>
2024-07-30 06:07:06 +0000

    Exclude endTime from active time interval of point source
    See merge request icon/icon-nwp!1474
    
    New option to exclude endTime from active (event) time interval of point source, `[startTime,endTime] -> [startTime,endTime)`.  
    Update of ART external including various other changes (Ozone, NetCDF, cosmetics).
    
    - See related ART merge request art/art!99.<br>
    - Introduction of namelist switch `lart_excl_end_pntSrc` in `NAMELIST /art_nml/`.
    
    Approved-by: Christoph Siewert <christoph.siewert@dwd.de>
    Merged-by: Florian Prill <florian.prill@dwd.de>

--- src/configure_model/mo_art_config.f90
+++ src/configure_model/mo_art_config.f90
@@ -56,6 +56,7 @@ MODULE mo_art_config
       &  cart_fplume_inp                 !< Path to FPlume input files (insert without extension)
     LOGICAL :: lart_diag_out             !< Enable output of diagnostic fields
     LOGICAL :: lart_pntSrc               !< Enables point sources
+    LOGICAL :: lart_excl_end_pntSrc      !< Main switch to exclude endTime from active time interval of point sources
     LOGICAL :: lart_emiss_turbdiff       !< Switch if emission should be included as surface flux condition
     CHARACTER(LEN=20) :: cart_io_suffix  !< user given suffix instead of automatically generated grid number 
                                          !  in ICON-ART input filename convention: 
