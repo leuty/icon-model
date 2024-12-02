5b24bc1f9c6d07bbd4bb64ad11fc89c59ac2931e
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2024-12-02 09:21:32 +0000

    CI: update the General Style checks
    https://gitlab.dkrz.de/icon/icon/-/merge_requests/470
    
    This updates the General Style CI job:
    1. The OpenMP sentinel check is rewritten in Python to improve maintainability.
    2. The CI job synchronizes only those submodules that undergo the checks.
    3. Run the encoding check for art and jsbach.
    4. Run the file permission check for jsbach.
    
    Approved-by: Ralf Mueller <ralf.mueller@dkrz.de>
    Merged-by: Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>

---
+++ scripts/ci_scripts/check_fortran.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+# ICON
+#
+# ---------------------------------------------------------------
+# Copyright (C) 2004-2024, DWD, MPI-M, DKRZ, KIT, ETH, MeteoSwiss
+# Contact information: icon-model.org
+#
+# See AUTHORS.TXT for a list of authors
+# See LICENSES/ for license information
+# SPDX-License-Identifier: BSD-3-Clause
+# ---------------------------------------------------------------
+
+import argparse
+import fnmatch
+import multiprocessing
+import os
+import re
+import sys
+
+FORTRAN_GLOB_PATTERNS = ["*.F90", "*.f90"]
+
+
+def list_files(dirs_or_files):
+    for dir_or_file in dirs_or_files:
+        if not os.path.exists(dir_or_file):
+            raise Exception(f"ERROR: '{dir_or_file}' does not exist")
+        elif os.path.isfile(dir_or_file):
+            yield dir_or_file
+        elif os.path.isdir(dir_or_file):
+            for subdir, _, filenames in os.walk(dir_or_file):
+                for filename in filenames:
+                    filepath = os.path.join(subdir, filename)
+                    if any(
+                        fnmatch.fnmatch(filepath, pattern)
+                        for pattern in FORTRAN_GLOB_PATTERNS
+                    ):
+                        yield filepath
+        else:
+            print(
+                f"WARNING: input argument '{dir_or_file}' "
+                f"is neither a directory nor a file",
+                file=sys.stderr,
+            )
+
+
+def check_file(filepath):
+    with open(filepath, "rb") as f:
+        raw = f.read(-1)
+        txt = raw.decode("utf-8", errors="replace")
+
+        # OpenMP sentinels (!$) are not allowed:
+        omp_sentinel_match = re.search(r"(?m)^\s*!\$\s.*$", txt)
+
+        return filepath, omp_sentinel_match.group() if omp_sentinel_match else None
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Checks Fortran source files for discouraged patterns"
+    )
+
+    parser.add_argument(
+        "files_or_dirs",
+        nargs="+",
+        metavar="FILE_OR_DIRECTORY",
+        help="path to a file or directory to check",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    files_with_omp_sentinels = []
+    with multiprocessing.Pool() as pool:
+        for (
+            filepath,
+            omp_sentinel_string,
+        ) in pool.imap_unordered(check_file, list_files(args.files_or_dirs)):
+            if omp_sentinel_string:
+                files_with_omp_sentinels.append((filepath, omp_sentinel_string))
+
+    exit_code = 0
+
+    if files_with_omp_sentinels:
+        exit_code = 1
+        print(
+            "ERROR: the following files contain OpenMP conditional compilation "
+            "sentinels:\n"
+            "\t{0}\n"
+            "Replace the sentinels (!$) in the files above with the macro "
+            "'#ifdef _OPENMP/#endif' directives.\n".format(
+                "\n\t".join(
+                    "{0}:\n{1}".format(n, s)
+                    for n, s in sorted(files_with_omp_sentinels)
+                )
+            ),
+            file=sys.stderr,
+        )
+
+    return exit_code
+
+
+if __name__ == "__main__":
+    sys.exit(main())
