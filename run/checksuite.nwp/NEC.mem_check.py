7b39c9805ecdbb4c9698a1d94dc880c7801d71fc
Marek Jacob <1129-b380572@users.noreply.gitlab.dkrz.de>
2025-02-03 09:28:26 +0000

    Check Memory consumption on NEC
    See merge request icon/icon-nwp!1399
    
    ## Feature Description
    This MR analyzes the memory report from the NEC `mpirun` to make sure that the memory consumption on the VE does not increase significantly without notice. This MR implements an upper and lower mem check. The lower mem check is to make sure that significant optimization is detected so that the limits can be adjusted thoroughly.
    
    The memory monitoring of the NEC is based on sampling. Therefore, the reporting is stochastic. To reduce the number of false alarms, the test run is repeated once if a threshold is exceeded.
    
    The limits are based on 172, 533, and 160 runs of experiments 03, 09, and 14, respectively. I choose the minimum of all runs of each experiment as lower limit and  216/215 times the minimum as the upper limit. (The factor is based on the observation that the memory footprint of ICON is approximately on the order of 215 3D fields)
    
    (This MR also fixes the OMP tests on the hybrid builder after renaming of the builders)
    
    Approved-by: Guenther Zaengl <guenther.zaengl@dwd.de>
    Merged-by: Guenther Zaengl <guenther.zaengl@dwd.de>

---
+++ run/checksuite.nwp/NEC.mem_check.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+# ICON
+#
+# ---------------------------------------------------------------
+# Copyright (C) 2004-2024, DWD, MPI-M, DKRZ, KIT, ETH, MeteoSwiss
+# Contact information: icon-model.org
+#
+# See AUTHORS.TXT for a list of authors
+# See LICENSES/ for license information
+# SPDX-License-Identifier: BSD-3-Clause
+# ---------------------------------------------------------------
+
+import argparse
+import os
+import sys
+
+needle = "Memory Size Used (MB) Min"
+
+
+def error(message, exit=1):
+    sys.stdout.flush()
+    sys.stderr.write(os.path.basename(sys.argv[0]) + ": " + message + "\n")
+    sys.stderr.flush()
+    sys.exit(exit)
+
+
+def scan_stdin():
+    mem = -1
+    for line in sys.stdin:
+        sys.stdout.write(line)  # forward line to Job log
+
+        # Look for
+        # Memory Size Used (MB) Min               :    48517.996
+        if line.startswith(needle):
+            first, second = line.split(":")
+            second = second.strip().split(" ")[0]
+            mem = max(mem, float(second))
+
+    if mem == -1:
+        error("Could not find 'Memory Size Used'", exit=2)
+
+    return mem
+
+
+message_template = """
+###############################################################################
+{error}
+###############################################################################
+
+Possible reasons for increased memory consumption
+=================================================
+* You enabled additional output variables.
+* You allocated additional variables.
+* You reduced the total number of VEs used.
+
+Possible reasons for unexpected low consumption
+===============================================
+* You disabled additional output variables.
+* You removed variables.
+* You increased the total number of VEs used.
+
+General reasons
+===============
+* You changed the hardware platform (e.g. SX30 instead of SX10)
+* Update of external dependency
+* Bad luck with the sampling of memory usage. In this case, please try again or
+  consider increasing the limit for this experiment slightly.
+
+The limits are defined in the experiment under `run/checksuite.nwp/nwpexp...`
+Please keep the limits tight but practical. The goal of this test is to detect
+additional 3D-variables which use about 180 to 220 MB.
+
+###############################################################################
+{error}
+###############################################################################
+"""
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Check the memory usage reported by NEC mpi"
+    )
+    parser.add_argument(
+        "--upper_mem_limit", default=0, help="Upper limit of the " + needle, type=int
+    )
+    parser.add_argument(
+        "--lower_mem_limit", default=0, help="Lower limit of the " + needle, type=int
+    )
+
+    args = parser.parse_args()
+
+    mem = scan_stdin()
+
+    if args.upper_mem_limit != 0:
+        if mem > args.upper_mem_limit:
+            error(
+                message_template.format(
+                    error="'%s' is higher than expected. (actual: %g; limit: %g)"
+                    % (needle, mem, args.upper_mem_limit)
+                ),
+                exit=21,
+            )
+    if args.lower_mem_limit != 0:
+        if mem < args.lower_mem_limit:
+            error(
+                message_template.format(
+                    error="'%s' is lower than expected. (actual: %g; limit: %g)"
+                    % (needle, mem, args.lower_mem_limit)
+                ),
+                exit=21,
+            )
