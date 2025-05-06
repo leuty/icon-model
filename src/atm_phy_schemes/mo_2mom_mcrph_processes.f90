2e6f09e6be6a23d244d930caaf9c8c85dc01a1b9
Mikael Stellio <mikael.stellio@c2sm.ethz.ch>
2025-05-06 15:49:35 +0000

    [nwp] Fix "partially present" bug on GPU
    https://gitlab.dkrz.de/icon/icon-nwp/-/merge_requests/1742
    
    ### Short description
    
    This fixes an OpenACC bug in exp.mch_kenda-ch1_dev_small on Balfrin: 
    variable in data clause is partially present on the device: tab%ltable(:,:,:,:)
    
    
    Co-authored-by: Daniel Reinert <daniel.reinert@dwd.de>
    Co-authored-by: Mikael Stellio <mikael.stellio@meteoswiss.ch>
    Merged-by: Daniel Reinert <daniel.reinert@dwd.de>

--- src/atm_phy_schemes/mo_2mom_mcrph_processes.f90
+++ src/atm_phy_schemes/mo_2mom_mcrph_processes.f90
@@ -4779,11 +4779,9 @@ CONTAINS
     kstart = ik_slice(3)
     kend   = ik_slice(4)
 
-    !$ACC WAIT(1)
-    !$ACC EXIT DATA DETACH(tab%ltable, tab%x1, tab%x2, tab%x3, tab%x4) FINALIZE
-    !$ACC ENTER DATA ATTACH(tab%ltable, tab%x1, tab%x2, tab%x3, tab%x4)
-
-    !$ACC PARALLEL ASYNC(1) DEFAULT(PRESENT) CREATE(hilf1, hilf2, hilf3, hilf4)
+    !ACCWA (nvhpc 24.5): In the following parallel region tab%ltable needs to be explicitly
+    !                    flagged as PRESENT to avoid a "partially present" error (randomly occurring).
+    !$ACC PARALLEL ASYNC(1) DEFAULT(PRESENT) PRESENT(tab%ltable) CREATE(hilf1, hilf2, hilf3, hilf4)
     !$ACC LOOP SEQ
     DO k = kstart,kend
       kp1_fl = MIN(k+1,SIZE(atmo%rho,dim=2))
