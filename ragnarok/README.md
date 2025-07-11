3e6cebbcd87c74268e0b576d33ffe8c3d9b57950
Georgiana Mania <mania@dkrz.de>
2025-07-11 10:39:27 +0000

    [mpim] Setup C++ infrastructure for WarmWorld projects
    https://gitlab.dkrz.de/icon/icon-mpim/-/merge_requests/634
    
    Implement infrastructure support for C++/Kokkos in ICON
    
    Co-authored-by: Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
    Co-authored-by: Joerg Behrens <behrens@dkrz.de>
    Approved-by: Ralf Mueller <ralf.mueller@dkrz.de>
    Merged-by: Ralf Mueller <ralf.mueller@dkrz.de>

---
+++ ragnarok/README.md
@@ -0,0 +1,7 @@
+# ICON RAGNAROK
+
+C++ implementation of the AES Physics based on [Kokkos](https://github.com/kokkos/kokkos) Core programming model.
+
+## Usage
+
+The project is part of ICON and built by the ICON build system if the option `--enable-ragnarok` is turned on; it also handles the proper compilation flags needed to configure Kokkos backend (from `externals/kokkos` submodule).
---
+++ ragnarok/src/icon_bridge/mo_ragnarok.f90
@@ -0,0 +1,45 @@
+! ICON
+!
+! ---------------------------------------------------------------
+! Copyright (C) 2004-2025, DWD, MPI-M, DKRZ, KIT, ETH, MeteoSwiss
+! Contact information: icon-model.org
+!
+! See AUTHORS.TXT for a list of authors
+! See LICENSES/ for license information
+! SPDX-License-Identifier: BSD-3-Clause
+! ---------------------------------------------------------------
+
+MODULE mo_ragnarok
+  IMPLICIT NONE
+  PRIVATE
+  PUBLIC :: init_ragnarok, getKokkosVersion
+
+  INTERFACE
+    SUBROUTINE init_ragnarok() BIND(c, name="init_ragnarok")
+    END SUBROUTINE init_ragnarok
+
+    FUNCTION get_kokkos_version_c(len) RESULT(ret) BIND(c, name="retrieve_kokkos_version_c")
+      USE, INTRINSIC :: ISO_C_BINDING
+      INTEGER(kind=c_int), INTENT(inout) :: len
+      TYPE(c_ptr) :: ret
+    END FUNCTION get_kokkos_version_c
+
+  END INTERFACE
+
+  PUBLIC :: get_kokkos_version_c
+
+CONTAINS
+
+  SUBROUTINE getKokkosVersion(str)
+    USE, INTRINSIC :: ISO_C_BINDING
+    IMPLICIT NONE
+    CHARACTER(kind=c_char), POINTER, INTENT(out) :: str(:)
+
+    TYPE(c_ptr) :: str_c
+    INTEGER(c_int) :: len
+
+    str_c = get_kokkos_version_c(len)
+    CALL C_F_POINTER(str_c, str, (/len/))
+  END SUBROUTINE
+
+END MODULE mo_ragnarok
