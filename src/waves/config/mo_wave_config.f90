486b9222e81e4d3425455110fe90295317a9652c
Daniel Reinert <daniel.reinert@dwd.de>
2026-02-18 08:23:28 +0000

    [nwp] revise wave propagation testcase
    https://gitlab.dkrz.de/icon/icon-nwp/-/merge_requests/2014
    
    Merged-by: Daniel Reinert <daniel.reinert@dwd.de>

--- src/waves/config/mo_wave_config.f90
+++ src/waves/config/mo_wave_config.f90
@@ -115,8 +115,9 @@ MODULE mo_wave_config
     LOGICAL :: lwave_stress1   ! if .TRUE., calculate wave stress, first call
     LOGICAL :: lwave_stress2   ! if .TRUE., calculate wave stress, second call
 
-    REAL(wp) :: peak_u10, peak_v10 ! peak value (m/s) of 10 m U and V wind speed for test case
     REAL(wp) :: peak_lat, peak_lon ! geographical location (deg) of wind peak value
+    REAL(wp) :: peak_wsp10         ! peak value of 10m wind spead at (peak_lat, peak_lon)
+    REAL(wp) :: dir_wsp10          ! flow direction measured clockwise from true north (deg)
 
     REAL(wp) :: impl_fac       !! implicitness factor for total source function time integration
                                !! impl_fac=0.5 : second order Crank-Nicholson/trapezoidal scheme
