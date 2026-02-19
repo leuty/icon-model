7e61dc494c62907342e308f93e4869c5685c9cea
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2026-02-19 15:19:52 +0100

    Release icon-2025.10-2
    https://gitlab.dkrz.de/icon/icon/-/merge_requests/782
    
    Merged-by: Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>

--- doc/www/conf.py
+++ doc/www/conf.py
@@ -39,7 +39,7 @@ myst_enable_extensions = [
     "substitution",
 ]
 
-myst_substitutions = {"release": "2025.10-1"}
+myst_substitutions = {"release": "2025.10-2"}
 
 # Important: This needs to be updated with a new release.
 # Several URLs are built using this string
